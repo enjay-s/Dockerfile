af588c078c440558d0a73a9e29add08df48463f2
宋恩杰 <songenjie@PC-20191030ZBDR.ljstaff.com>
2024-12-05 16:14:27 +0800

    [Dockerfile][Feature][ISSUE#2] dev support

---
+++ ByConity/Centos7/dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+systemctl daemon-reload
+systemctl enable vncserver.service
+systemctl start vncserver.service
+systemctl status vncserver.service
+
+#vncpasswd
+#firewall-cmd --permanent --add-port=5901/tcp
+#firewall-cmd --reload
+#firewall-cmd --permanent --add-service vnc-server
+#systemctl restart firewalld.service
+#netstat -lnpt|grep Xvnc
